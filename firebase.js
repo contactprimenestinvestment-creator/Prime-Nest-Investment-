00557c34331acdcde1dca030d6566e7bd59889fd
contactprimenestinvestment-creator <contactprimenestinvestment@gmail.com>
2025-08-07 01:21:13 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhs5TEBrTOYj3iq9FaMbbBVBkfkuhncAg",
+  authDomain: "primenest-5156e.firebaseapp.com",
+  projectId: "primenest-5156e",
+  storageBucket: "primenest-5156e.appspot.com",
+  messagingSenderId: "1075380637024",
+  appId: "1:1075380637024:web:e415ad34d878a4a67f2205"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);￼Enter
